3aeb360f92fb0ee8ab57c93dce6b74f68a970c0d
davenh99 <102002840+davenh99@users.noreply.github.com>
2025-07-17 09:59:11 +1000

    finalised initial types
    closes #1

--- ui/Types.ts
+++ ui/Types.ts
@@ -1,22 +1,31 @@
 export interface User {
   username: string;
   email: string;
+  dob: string;
+  height: string;
+  weight: string;
 }
 
 export interface MeasurementType {
   system: boolean;
   name: string;
+  public: boolean;
+  createdBy: string;
+  numeric: boolean;
 }
 
 export interface MeasurementValue {
   measurementType: string;
-  name: string;
+  value: string;
+  public: boolean;
+  createdBy: string;
 }
 
 export interface Session {
   name: string;
   description: string;
   usersSaved: string[];
+  public: boolean;
 }
 
 export interface Exercise {
@@ -30,7 +39,7 @@ export interface Exercise {
 export interface SessionExercise {
   exercise: string;
   setCount: number;
-  dropSetParent: string; // TODO rename
+  supersetParent: string | null;
 }
 
 export interface UserSession {
@@ -39,22 +48,27 @@ export interface UserSession {
   userDay: string;
   notes: string;
   tags: string[];
+  userHeight: number;
+  userWeight: number;
 }
 
 export interface UserSessionExercise {
   user: string;
   exercise: string;
+  userSession: string;
   notes: string;
   tags: string[];
   addedWeight: number;
   qty: number;
-  measurementValue: string;
   restAfter: number;
-  sessionExercise?: string;
-  dropSetParent?: string; // TODO rename
+  measurementNumeric: number | null;
+  measurementValue: string | null;
+  sessionExercise: string | null;
+  supersetParent: string | null;
 }
 
 export interface tag {
   name: string;
   user: string;
+  public: boolean;
 }
