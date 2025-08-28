f3c73aadb46901b3f21e42aa6f7e8fcc2d5f8f43
davenh99 <102002840+davenh99@users.noreply.github.com>
2025-08-29 08:49:22 +1000

    simplify add exercise migrations

--- db/dist/pb_migrations/4_add_exercises.js
+++ db/dist/pb_migrations/4_add_exercises.js
@@ -6,28 +6,22 @@ migrate(
     const newExercises = [
       {
         id: "70vlzfveuudrmax",
-        s: true,
         n: "20mm Edge lift",
         mType: "8ldlggjjvy2ircl",
-        p: true,
         b: false,
         d: "A one-hand lift on a 20mm edge, an excellent choice for building finger strength. Select from grip type.",
       },
       {
         id: "a5wjd65uffrjt8f",
-        s: true,
         n: "Pullup",
         mType: "m01s8yx7wfk2gxd",
-        p: true,
         b: true,
         d: "Select from grip type.",
       },
       {
         id: "a5wjd65ifgrjt8f",
-        s: true,
-        n: "Boulder (hueco)",
+        n: "Boulder",
         mType: "8ldlgghjvy4ircl",
-        p: true,
         b: true,
         d: "Select grade",
       },
@@ -37,10 +31,10 @@ migrate(
       let record = new Record(exercises);
 
       record.set("id", e.id);
-      record.set("system", e.s);
+      record.set("system", true);
       record.set("name", e.n);
       record.set("measurementType", e.mType);
-      record.set("public", e.p);
+      record.set("public", true);
       record.set("bodyweight", e.b);
       record.set("description", e.d);
 
