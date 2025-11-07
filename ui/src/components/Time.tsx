a57e75a1bcc4b78392210424c24a27f5ef14793c
David <davidham975@gmail.com>
2025-11-07 19:03:11 +1100

    lovely time input closes #110

--- ui/src/components/Time.tsx
+++ ui/src/components/Time.tsx
@@ -5,11 +5,16 @@ interface Props {
   value: number;
   onValueChange: (v: number) => void;
   saveFunc: (v: number) => Promise<any>;
+  maxMinutes?: number;
 }
 
 export const DataTime: Component<Props> = (props) => {
   const [minutes, setMinutes] = createSignal(Math.floor(props.value / 60));
   const [seconds, setSeconds] = createSignal(Math.round(props.value % 60));
+  const [timesEnteredMinutes, setTimesEnteredMinutes] = createSignal(0);
+  const [timesEnteredSeconds, setTimesEnteredSeconds] = createSignal(0);
+  let minRef: HTMLInputElement;
+  let secRef: HTMLInputElement;
 
   createEffect(() => {
     const min = minutes() || 0;
@@ -20,29 +25,37 @@ export const DataTime: Component<Props> = (props) => {
     props.saveFunc(newVal).then(() => props.onValueChange(newVal));
   });
 
-  // TODO this was making a loop, do I need the code and how to avoid the loop?
-  // not onMount, should be reactive to external state changes...
-  // createEffect(() => {
-  //   setMinutes(Math.floor(props.value / 60));
-  //   setSeconds(Math.round(props.value % 60));
-  // });
-
   return (
     <div class="px-1 flex items-center">
       <NumberInput
         width="1.8rem"
-        maxValue={999}
+        maxValue={props.maxMinutes ?? 999}
         minValue={0}
         inputProps={{
+          ref: (el) => (minRef = el),
+          onFocus: (e) => e.currentTarget.select(),
           value: minutes(),
           onInput: (e) => {
+            if (e.currentTarget.value.includes(".")) {
+              e.currentTarget.value = e.currentTarget.value.replace(".", "");
+              setTimesEnteredMinutes(0);
+              secRef?.focus();
+              return;
+            }
+
             const raw = parseInt(e.currentTarget.value) || 0;
             const clamped = Math.max(0, Math.min(raw, 999));
-            setMinutes(clamped);
 
             if (clamped !== raw) {
               e.currentTarget.value = clamped.toString();
             }
+
+            setTimesEnteredMinutes(timesEnteredMinutes() + 1);
+            if (timesEnteredMinutes() >= (props.maxMinutes ?? 999).toString().length) {
+              setTimesEnteredMinutes(0);
+              secRef.focus();
+            }
+            setMinutes(clamped);
           },
         }}
       />
@@ -52,15 +65,23 @@ export const DataTime: Component<Props> = (props) => {
         maxValue={59}
         minValue={0}
         inputProps={{
+          ref: (el) => (secRef = el),
+          onFocus: (e) => e.currentTarget.select(),
           value: seconds().toString().padStart(2, "0"),
           onInput: (e) => {
             const raw = parseInt(e.currentTarget.value) || 0;
             const clamped = Math.min(59, Math.max(0, raw));
-            setSeconds(clamped);
 
             if (clamped !== raw) {
               e.currentTarget.value = clamped.toString();
             }
+
+            setTimesEnteredSeconds(timesEnteredSeconds() + 1);
+            if (timesEnteredSeconds() >= 2) {
+              setTimesEnteredSeconds(0);
+              secRef.blur();
+            }
+            setSeconds(clamped);
           },
         }}
       />
