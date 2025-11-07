21be24974b7b2af4116e5f8b9ec5680372f3609b
David <davidham975@gmail.com>
2025-11-07 16:19:45 +1100

    updated view collection to include fields in original collection

--- dist/pb_migrations/0021_created_exerciseVariations_computed.js
+++ dist/pb_migrations/0021_created_exerciseVariations_computed.js
@@ -22,6 +22,20 @@ migrate(
         {
           autogeneratePattern: "",
           hidden: false,
+          id: "_clone_huYE",
+          max: 0,
+          min: 0,
+          name: "name",
+          pattern: "",
+          presentable: false,
+          primaryKey: false,
+          required: false,
+          system: false,
+          type: "text",
+        },
+        {
+          autogeneratePattern: "",
+          hidden: false,
           id: "text1731158936",
           max: 0,
           min: 0,
@@ -33,23 +47,50 @@ migrate(
           system: false,
           type: "text",
         },
+        {
+          autogeneratePattern: "",
+          hidden: false,
+          id: "_clone_C0R0",
+          max: 0,
+          min: 0,
+          name: "description",
+          pattern: "",
+          presentable: false,
+          primaryKey: false,
+          required: false,
+          system: false,
+          type: "text",
+        },
+        {
+          cascadeDelete: true,
+          collectionId: "pbc_1804250889",
+          hidden: false,
+          id: "_clone_S2P8",
+          maxSelect: 1,
+          minSelect: 0,
+          name: "exercise",
+          presentable: false,
+          required: true,
+          system: false,
+          type: "relation",
+        },
       ],
-      id: "pbc_98694773",
+      id: "pbc_754532938",
       indexes: [],
-      listRule: "",
-      name: "exerciseVariations_computed",
+      listRule: null,
+      name: "exerciseVariations_view",
       system: false,
       type: "view",
       updateRule: null,
       viewQuery:
-        "SELECT\n  (ROW_NUMBER() OVER()) as id,\n  CAST((exercises.name || ' (' || exerciseVariations.name || ')') AS TEXT) displayName\nFROM exerciseVariations\nJOIN exercises ON exerciseVariations.exercise = exercises.id",
-      viewRule: "",
+        "SELECT\n  v.id AS id,\n  v.name AS name,\n  CAST((e.name || ' (' || v.name || ')') AS TEXT) displayName,\n  v.description AS description,\n  v.exercise AS exercise\nFROM exerciseVariations v\nJOIN exercises e ON v.exercise = e.id",
+      viewRule: null,
     });
 
     return app.save(collection);
   },
   (app) => {
-    const collection = app.findCollectionByNameOrId("pbc_98694773");
+    const collection = app.findCollectionByNameOrId("pbc_754532938");
 
     return app.delete(collection);
   }
